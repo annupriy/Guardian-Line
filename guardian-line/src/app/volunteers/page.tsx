cbc3c0064884c7b2ab4c63524fe2115a6dfee0d1
Annu Priya <anuthisisme@gmail.com>
2024-02-14 01:19:39 +0530

    Cleaned up

---
+++ guardian-line/src/app/volunteers/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Button} from "@nextui-org/react";
+
+const accessingLocation = () => {
+  return (
+    <div className="flex flex-wrap gap-4 items-center p-4">
+    <Button color="primary">
+        Accessing location
+    </Button>   
+    </div>
+  )
+}
+
+export default accessingLocation
