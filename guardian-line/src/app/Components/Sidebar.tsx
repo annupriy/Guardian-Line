7ec475db8230f9bf2c4e95966b63642af2af824d
Annu Priya <anuthisisme@gmail.com>
2024-02-19 01:11:33 +0530

    UI Changes

--- guardian-line/src/app/Components/Sidebar.tsx
+++ guardian-line/src/app/Components/Sidebar.tsx
@@ -4,21 +4,21 @@ import Link from "next/link";
 const Sidebar = () => {
   return (
     <>
-      <ul className="menu menu-md h-full mx-2 flex-nowrap justify-evenly">
+      <ul className="menu menu-md h-full mx-2 flex-nowrap justify-evenly bg-stone-400	">
         <li className="pt-2 focus:outline-none focus:ring text-cyan-800">
-        <Link href="/volunteers" className="text-lg">Reported Crimes</Link>
+        <Link href="/volunteers" className="text-lg text-white">Reported Crimes</Link>
         </li>
         <li className="focus:outline-none focus:ring text-cyan-800">
-        <Link href="/volunteers" className="text-lg">Volunteers page</Link>
+        <Link href="/volunteers" className="text-lg text-white">Volunteers page</Link>
         </li>
         <li className="focus:outline-none focus:ring text-cyan-800">
-        <Link href="/volunteers" className="text-lg">About Us</Link>
+        <Link href="/volunteers" className="text-lg text-white">About Us</Link>
         </li>
         <li className="focus:outline-none focus:ring text-cyan-800">
-        <Link href="/volunteers" className="text-lg">User Guide</Link>
+        <Link href="/volunteers" className="text-lg text-white">User Guide</Link>
         </li>
         <li className="focus:outline-none focus:ring text-cyan-800">
-        <Link href="/volunteers" className="text-lg">Contact Us</Link>
+        <Link href="/volunteers" className="text-lg text-white">Contact Us</Link>
         </li>
       </ul>
     </>
