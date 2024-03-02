c04e17e93bc3f9b6f2a7d579aedcb8498075efb8
Annu Priya <anuthisisme@gmail.com>
2024-03-02 14:59:44 +0530

    FIxed mongodb connection issue.

--- guardian-line/src/app/lib/mongodb.js
+++ guardian-line/src/app/lib/mongodb.js
@@ -1,17 +1,21 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URL
+const uri = process.env.MONGODB_URL;
 const options = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
-}
+};
 
-let client
-let clientPromise
+let client;
+let clientPromise;
 
 if (!process.env.MONGODB_URL) {
-  throw new Error('Add Mongo URI to .env.local')
+  throw new Error("Add Mongo URI to .env.local");
 }
-  client = new MongoClient(uri, options)
-  clientPromise = client.connect()
-export default clientPromise
+
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri, options);
+  global._clientPromise = client.connect();
+}
+clientPromise = global._clientPromise;
+export default clientPromise;
