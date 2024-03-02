e9293dc7ea6e71fdc6b53b3fd1cb0d09c8f2134d
bhatialovisha <96584269+bhatialovisha@users.noreply.github.com>
2024-03-02 23:51:44 +0530

    Adding location feature in Reported crimes

--- guardian-line/src/app/(logged)/reported_crimes/reportsFiled.tsx
+++ guardian-line/src/app/(logged)/reported_crimes/reportsFiled.tsx
@@ -1,26 +1,228 @@
-import React from 'react';
+import React, { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { MapPinIcon } from "@heroicons/react/24/outline";
+export type IncidentLocation = {
+  latitude: number;
+  longitude: number;
+  address: string;
+};
 
-const ReportsFiled = () => {
+type ActiveCrimesCardsProps = {
+  descriptionOfIncident: string;
+  incidentLocation: IncidentLocation;
+  personalInformation: string;
+  timeOfIncident: string;
+  userName: string;
+  reportid: string;
+  typeOfIncident: string;
+  distance: string;
+};
+
+const ActiveCrimesCards: React.FC<ActiveCrimesCardsProps> = ({
+  typeOfIncident,
+  distance,
+  descriptionOfIncident,
+  incidentLocation,
+  personalInformation,
+  timeOfIncident,
+  userName,
+  reportid,
+}) => {
+  const [showButtons1, setShowButtons1] = useState(true);
+  const [showButtons2, setShowButtons2] = useState(false);
+  const [showCard, setShowCard] = useState(true);
+  const [showThanksMessage, setShowThanksMessage] = useState(false);
+
+  const handleGoingToValidate = () => {
+    setShowButtons2(true);
+    setShowButtons1(false);
+    handleReportClick();
+  };
+
+  const handleSkip = () => {
+    setShowCard(false);
+    removeCrimeReport();
+  };
+
+  const handleDone = () => {
+    setShowCard(false);
+    setShowThanksMessage(true);
+  };
+
+  const handleReportClick = () => {
+    // Hide the message after 5 seconds
+    setTimeout(() => {
+      setShowThanksMessage(false);
+    }, 5000);
+  };
+
+  const removeCrimeReport = async () => {
+    // Remove the object from ActiveCrimes from ActiveVolunteers Collection
+    const res = await fetch("/api/volunteersLocation", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        removeReport: true,
+        reportid: reportid,
+        vote: 0,
+      }),
+    });
+    if (!res) {
+      toast.dismiss();
+      toast.error("Error");
+    } else if (res.status === 200) {
+      toast.dismiss();
+      console.log("Deleted");
+    } else {
+      toast.dismiss();
+      if (res.status === 401) {
+        toast.error("Error skipping report. Please try again.");
+      } else {
+        toast.error("Could not skip report");
+      }
+    }
+  };
+
+  const handleVoteYes = async () => {
+    // Remove the object from ActiveCrimes from ActiveVolunteers Collection
+    const res = await fetch("/api/volunteersLocation", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        vote: 1,
+        removeReport: true,
+        reportid: reportid,
+      }),
+    });
+    if (!res) {
+      toast.dismiss();
+      toast.error("Error");
+    } else if (res.status === 200) {
+      toast.dismiss();
+      console.log("Deleted");
+    } else {
+      toast.dismiss();
+      if (res.status === 401) {
+        toast.error("Error validating report. Please try again.");
+      } else {
+        toast.error("Could not validate");
+      }
+    }
+  };
+
+  const handleVoteNo = async () => {
+    // Remove the object from ActiveCrimes from ActiveVolunteers Collection
+    const res = await fetch("/api/volunteersLocation", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        vote: -1,
+        removeReport: true,
+        reportid: reportid,
+      }),
+    });
+    if (!res) {
+      toast.dismiss();
+      toast.error("Error");
+    } else if (res.status === 200) {
+      toast.dismiss();
+      console.log("Deleted");
+    } else {
+      toast.dismiss();
+      if (res.status === 401) {
+        toast.error("Error validating report. Please try again.");
+      } else {
+        toast.error("Could not validate");
+      }
+    }
+  };
+
+  const latitude = incidentLocation.latitude;
+  const longitude = incidentLocation.longitude;
+  const address = incidentLocation.address;
+
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {[...Array(5)].map((_, index) => (
-          <div key={index} className="max-w-xl bg-white shadow-lg rounded-lg overflow-hidden">
-            <div className="px-6 py-4">
-              <h2 className="text-2xl font-bold mb-2">Date</h2>
-              <p className="text-gray-700">Description of the report</p>
+    <>
+      {showCard && (
+        <div className="card w-96 bg-[darkslategray] text-neutral-content self-start threedbox">
+          <div className="card-body flex-nowrap">
+            <div className="card-actions justify-between flex-nowrap items-center ">
+              <div className="card-title text-2xl">{typeOfIncident}</div>
+              {/* show distance upto 2 decimal digits only */}
+              <div className="card-title text-xl">
+                {parseFloat(distance).toFixed(2)}
+                {" km"}
+                <MapPinIcon className="w-6 rounded-full overflow-hidden border-cyan-900"></MapPinIcon>
+              </div>
             </div>
-            <div className="px-6 py-4 flex justify-end">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                View Full Report
-              </button>
+            <div className="items-start">
+            <div className="italic">Description: {descriptionOfIncident}</div></div>
+            <div className="collapse collapse-arrow">
+              <input type="checkbox" />
+              <div className="collapse-title text-xl font-medium flex justify-between ">
+                Address
+                <div className="card-title text-sm">Time: {timeOfIncident}</div>
+              </div>
+              <div className="collapse-content border-transparent bg-[#e7c8c2] text-black rounded-xl ">
+                <p className = "mt-3">{address}</p>
+                <h5 className="mt-2">Latitude: {latitude}</h5>
+                <h5 className="">Longitude: {longitude}</h5>
+              </div>
             </div>
+            <div className="mb-3">
+              <p>Personal Information: {personalInformation}</p>
+            </div>
+            {showButtons1 && (
+              <div className="card-actions justify-between flex-nowrap">
+                <button
+                  className="btn bg-amber-700"
+                  onClick={handleGoingToValidate}
+                >
+                  Going to validate or help?
+                </button>
+                <button className="btn btn-ghost" onClick={handleSkip}>
+                  Skip this one
+                </button>
+              </div>
+            )}
+            {showButtons2 && (
+              <div className="card-actions justify-evenly flex-nowrap">
+                <button
+                  className="btn btn-primary"
+                  onClick={() => {
+                    handleDone();
+                    handleVoteYes();
+                  }}
+                >
+                  True Report
+                </button>
+                <button
+                  className="btn btn-ghost"
+                  onClick={() => {
+                    handleDone();
+                    handleVoteNo();
+                  }}
+                >
+                  False Report
+                </button>
+              </div>
+            )}
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      )}
+      {showThanksMessage && (
+        <p className="text-green-600 mt-4">Thanks for helping us validate!</p>
+      )}
+    </>
   );
 };
 
-export default ReportsFiled;
+
+export default ActiveCrimesCards;
 
